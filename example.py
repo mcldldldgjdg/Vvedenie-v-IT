a13f7466e4168619e7439078631f7c6b41e7de12
mcldldldgjdg <105590869+mcldldldgjdg@users.noreply.github.com>
2025-09-26 20:30:47 +0300

    задание 19.09.25

---
+++ example.py
@@ -0,0 +1,14 @@
+#1
+x=int(input())
+for i in range(1,x+1):
+    print(i)
+
+#2
+k=int(input("1 number "))
+l=int(input("2 number "))
+if k>l:
+    print("bigger number is - ", k)
+elif k<l:
+    print("bigger number is - ", l)
+else :
+    print("equal")
